12c64fc723c77df5c6ec241d041bf9de248ece15
Jason C. Liu <32473162+TropicalSunshine@users.noreply.github.com>
2018-09-20 20:55:47 -0700

    added animations
    using the scrollreveal library

--- js/Scrollrevealconfig.js
+++ js/Scrollrevealconfig.js
@@ -1,5 +1,137 @@
-ScrollReveal().reveal(".fadeIn", {
+
+ScrollReveal().reveal(".showcase-text h6", {
+    delay: 400,
+});
+
+ScrollReveal().reveal(".showcase-text h1", {
+    delay: 1100
+});
+
+ScrollReveal().reveal(".showcase-text p", {
+    delay: 1500
+});
+
+ScrollReveal().reveal(".showcase-text a", {
+    delay: 2000
+});
+
+ScrollReveal().reveal(".about-me-box img", {
     delay: 300,
-    orgin: 'top',
-    distance: '300px'
+    orgin: 'bottom',
+    distance: '30px'
+});
+
+ScrollReveal().reveal(".about-me-box .section-header-font", {
+    delay: 350,
+    origin: 'bottom',
+    distance: "40px"
+});
+
+ScrollReveal().reveal(".aboutme-descrip-one", {
+    delay: 350,
+    origin: "bottom",
+    distance:  "40px"
+});
+
+ScrollReveal().reveal(".aboutme-descrip-two", {
+    delay: 350,
+    origin: "bottom",
+    distance: "40px"
+});
+
+ScrollReveal().reveal("#projects .section-header-font", {
+    delay: 350,
+});
+
+ScrollReveal().reveal("#projects .projects-content-box", {
+    delay: 350,
+});
+
+ScrollReveal().reveal("#blog .section-header-font", {
+    delay: 350,
+    origin: 'bottom',
+    distance: "40px"
+});
+
+ScrollReveal().reveal("#blog p", {
+    delay: 500,
+});
+
+ScrollReveal().reveal("#blog .blog-content-box-description", {
+    delay: 500,
+    origin: 'bottom',
+    distance: "40px"
+});
+
+ScrollReveal().reveal("#work h1", {
+    delay: 350,
+    origin: 'bottom',
+    distance: "40px"
+});
+
+ScrollReveal().reveal("#work h2", {
+    delay: 350,
+    origin: 'bottom',
+    distance: "40px"
+});
+
+ScrollReveal().reveal("#work .box-one", {
+    delay: 500,
+});
+
+ScrollReveal().reveal("#work .box-two", {
+    delay: 800,
+});
+
+ScrollReveal().reveal("#work .box-three", {
+    delay: 1100,
+});
+
+ScrollReveal().reveal("#gallery .section-header-font", {
+    delay: 350,
+    origin: 'bottom',
+    distance: "40px"
+});
+
+ScrollReveal().reveal("#gallery h2", {
+    delay: 370,
+    origin: 'bottom',
+    distance: "40px"
+});
+
+ScrollReveal().reveal("#gallery .gallery-item", {
+    origin: 'bottom',
+    distance: "40px"
+});
+
+ScrollReveal().reveal("#contact h1", {
+    delay: 350,
+    origin: 'bottom',
+    distance: "40px"
+});
+
+ScrollReveal().reveal("#contact .contact-text-box-bottom", {
+    delay: 350,
+    origin: 'bottom',
+    distance: "40px"
+});
+
+ScrollReveal().reveal("#contact .fa-facebook", {
+    delay: 300
+});
+
+ScrollReveal().reveal("#contact .fa-linkedin", {
+    delay: 600
+});
+
+ScrollReveal().reveal("#contact .fa-envelope-o", {
+    delay: 900
+});
+
+ScrollReveal().reveal("#contact .fa-github", {
+    delay: 1200
+});
+
+ScrollReveal().reveal("#contact .fa-instagram", {
+    delay: 1500
 });
